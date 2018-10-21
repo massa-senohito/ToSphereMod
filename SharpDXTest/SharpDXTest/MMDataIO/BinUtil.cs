85e8e17c5685cbef73aea39ebb58f68cb3efe962
massa-senohito <i7da.masa@gmail.com>
2018-10-22 00:04:06 +0900

    改造したMMDataIOを追加

---
+++ SharpDXTest/SharpDXTest/MMDataIO/BinUtil.cs
@@ -0,0 +1,42 @@
+﻿using SharpDX;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMDataIO
+{
+	public static class BinUtil
+	{
+		public static void WriteV( this BinaryWriter writer ,Vector2 v)
+		{
+			writer.Write( v.X );
+			writer.Write( v.Y );
+		}
+
+		public static void WriteV( this BinaryWriter writer ,Vector3 v)
+		{
+			writer.Write( v.X );
+			writer.Write( v.Y );
+			writer.Write( v.Z );
+		}
+
+		public static void WriteV( this BinaryWriter writer ,Vector4 v)
+		{
+			writer.Write( v.X );
+			writer.Write( v.Y );
+			writer.Write( v.Z );
+			writer.Write( v.W );
+		}
+
+		public static void WriteQ( this BinaryWriter writer ,Quaternion v)
+		{
+			writer.Write( v.X );
+			writer.Write( v.Y );
+			writer.Write( v.Z );
+			writer.Write( v.W );
+		}
+	}
+}
